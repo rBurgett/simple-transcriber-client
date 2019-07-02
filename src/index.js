4e7a097c479c1c0c66fa2846b1325838ea447544
rBurgett <ryan@burgettweb.net>
2019-07-02 02:32:04 -0400

    Added babel, eslint, react, and electron

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1><i className="fas fa-check" /> Simple Transcriber</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('js-main')
+);
